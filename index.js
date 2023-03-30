685a1f02174701febf898e3cdffd2c59a8bfba02
mahziar saadat <mahzyarsaadat764@gmail.com>
2023-03-30 14:16:49 +0430

    final-project

---
+++ index.js
@@ -0,0 +1,6 @@
+let menu = document.getElementById('menu');
+let navbar = document.getElementById('dif-scren-nav');
+
+menu.addEventListener('click' , e => {
+    navbar.classList.toggle('active')
+})
